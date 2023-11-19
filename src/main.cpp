5ba21669fae3ad4ab1a983f1b991a818b6195b92
diego59x <diegoalvarez1237@hotmail.com>
2023-11-19 15:37:12 -0600

    asd

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,6 @@
 #include "WillAgainst/WillAgainst.h"
 
 int main(int argc, char* argv[]) {
-    Pong pong = Pong("pong", 800, 800);
+    Pong pong = Pong("Will Little Ceasar", 800, 800);
     pong.run();
 };
